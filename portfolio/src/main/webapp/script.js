c6fb62c30458ae04f1995c69be14f12dafeaf78e
anitaHey <jiaa.anita@gmail.com>
2020-07-15 22:34:54 +0800

    Add image upload for my portfolio

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,11 +13,25 @@
 // limitations under the License.
 
 $( document ).ready(function() {
-    fetch('/list-message').then(response => response.json()).then((messages) => {
+    $("#input_form").hide();
+    $("#image_upload").hide();
+
+    fetch('/list-message')
+     .then(response => response.json()).then((messages) => {
         messages.forEach((message) => {
             $('#message_board').append(createMessageElement(message));
         });
-    });
+     });
+
+    fetch('/blobstore-upload-url')
+      .then((response) => {
+        return response.text();
+      })
+      .then((imageUploadUrl) => {
+        const messageForm = document.getElementById('input_form');
+        messageForm.action = imageUploadUrl;
+        $("#input_form").show();
+      });
 
     $('#first_next').click(function(){
         $('html, body').animate({
@@ -49,6 +63,11 @@ $( document ).ready(function() {
             scrollTop: $("#third").offset().top
         }, 1000);
     });
+
+    $('input[type="file"]').change(function(e){
+        var fileName = e.target.files[0].name;
+        $('#upload_image_name').text(fileName);
+    });
 });
 
 /** Creates an element that represents a message. */
@@ -65,9 +84,24 @@ function createMessageElement(message) {
     message_last.appendChild(message_name);
     message_last.appendChild(message_time);
 
+    const message_text = document.createElement('div');
+    message_text.className = 'message_text';
+    message_text.innerText = message.content;
+
     const message_content = document.createElement('div');
     message_content.className = 'message_content';
-    message_content.innerText = message.content;
+    message_content.appendChild(message_text);
+
+    if(message.image_url){
+        const message_image = document.createElement('img');
+        message_image.className = 'message_image';
+        message_image.src = message.image_url;
+
+        const message_image_div = document.createElement('div');
+        message_image_div.className = 'message_image_div';
+        message_image_div.appendChild(message_image);
+        message_content.appendChild(message_image_div);
+    }
 
     const message_list = document.createElement('div');
     message_list.className = 'message_list';
